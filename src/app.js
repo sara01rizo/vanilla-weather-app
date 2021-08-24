cb919352a37ddc6cfc8fa696e8e4a89659450a98
Sara Rizo <sara01rizo@gmail.com>
2021-08-24 17:31:20 +0200

    Added axios in html

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-alert("Hello world");
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
+let apiKey = "6689484e79257d67b075593fc22c1ad7";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
